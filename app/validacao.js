044cab6a1739758d41db6f586256d68ee8c31877
Lucas Kaua Dias Cunha <lucasdcunha42@gmail.com>
2023-03-27 02:38:35 -0300

    gameOver option

--- app/validacao.js
+++ app/validacao.js
@@ -2,8 +2,21 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute
 
     if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Valor inválido</div>'
-        return
+        if (chuteForInvalido(numero)) {
+            if (chute.toUpperCase() === "GAME OVER") {
+    
+                document.body.innerHTML =
+                    `
+                    <h1>Game Over!!!</h1>
+                    <h3>Pressione o botão para jogar novamente</h3>
+                    <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                    `
+                    document.body.style.backgroundColor = "black";
+            } else {
+    
+                elementoChute.innerHTML += '<div>Valor Inválido</div>';
+            }
+        }
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
